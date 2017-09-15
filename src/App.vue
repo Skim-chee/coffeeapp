22ebc07efe5383f5d484f4a88ec94ab2f7da9f1f
skim-chee <kim.seean@gmail.com>
2017-09-14 23:58:32 -0400

    First commit

---
+++ src/App.vue
@@ -0,0 +1,166 @@
+<template>
+  <div id="app">
+    <img id = "logo" src="./assets/logo@2x.png">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang = "scss">
+@import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600');
+@import url('https://fonts.googleapis.com/css?family=Playfair+Display:900');
+
+/* Variables */
+
+$sans-serif: 'Open Sans', sans-serif;
+$serif: 'Playfair Display', serif;
+$black: #000;
+$white: #fff;
+$off-black: #333;
+$gray: #BBBBBB;
+$primary: #119AF7;
+$secondary: #FA1821;
+
+/* reset */
+
+* {
+  -webkit-box-sizing: border-box;
+     -moz-box-sizing: border-box;
+          box-sizing: border-box;
+       -webkit-margin-before:0em;
+        -webkit-margin-after:0em;
+}
+
+html {
+  overflow-x: hidden;
+}
+
+a {
+  text-decoration: none;
+}
+
+a,
+a:active,
+a:focus {
+  outline: none !important;
+  /* Works in Firefox, Chrome, IE8 and above */
+}
+button,
+button:active,
+button:focus {
+  outline: none !important;
+  /* Works in Firefox, Chrome, IE8 and above */
+}
+
+img {
+  &,
+  &:active,
+  &:hover {
+    outline: none !important;
+  }
+
+  display: block;
+}
+
+button {
+    border:none;
+    background-image:none;
+    background-color:transparent;
+    -webkit-box-shadow: none;
+    -moz-box-shadow: none;
+    box-shadow: none;
+    padding: 0;
+}
+
+html {
+  font-family: 'Open Sans', sans-serif;
+  color: $off-black;
+}
+
+/* Typography */
+h1{
+  font-family: $serif;
+  font-size: 72px;
+  font-weight: 900;
+}
+
+p {
+  font-size: 21px;
+  font-weight: 600;
+}
+
+.white {
+  color: #fff;
+}
+
+.primary {
+  color: $primary;
+  /*border-bottom: 1px solid $primary;*/
+}
+.secondary{
+  color: $secondary;
+}
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  display: flex;
+
+  flex-direction: column;
+  margin-top: 24px;
+
+  #logo {
+    height: 148px;
+    align-self: center;
+  }
+}
+
+.btn-primary {
+  height: 48px;
+  width: 192px;
+  margin-top: 48px;
+
+  background: $white;
+  border-radius: 2px;
+  border: 2px solid $primary;
+  text-align: center;
+
+  transform: translateY(0px);
+  transition: all .3s ease;
+
+  p {
+    color: $primary;
+  }
+
+  &:active, &:hover {
+    background: $primary;
+    transform: translateY(-3px);
+    transition: all .3s ease;
+    border-color: transparent;
+    box-shadow: 0 2px 2px $gray;
+
+    p {
+      color: $white;
+    }
+  }
+}
+
+.btn-secondary {
+  @extend .btn-primary;
+
+  border: 2px solid $secondary;
+  p {
+    color: $secondary;
+  }
+
+  &:active, &:hover {
+    background: $secondary;
+  }
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,167 @@
+
+<template>
+  <div class="body">
+    <h1>Stumptown Coffee</h1>
+
+    <form v-on:submit= "submit">
+      <div id = "selection">
+        <p>What type of day is it?</p>
+        <div class = "button-set">
+          <input id = "chill" name = "day" value = "Chill"  type = "radio" v-model = "optionOne">
+          <label class = "btn-image chill bounce" for = "chill"></label>
+
+          <input id = "productive" name = "day" value = "Productive"  type = "radio" v-model = "optionOne">
+          <label class = "btn-image productive" for = "productive"></label>
+        </div>
+
+        <p>Feeling adventurous?</p>
+        <div class = "button-set">
+          <input checked = "checked" id = "stay" name = "distance" value = "Stay"  type = "radio" v-model = "optionTwo">
+          <label class = "btn-image stay" for = "stay"></label>
+
+          <input id = "go" name = "distance" value = "Go"  type = "radio" v-model = "optionTwo">
+          <label class = "btn-image go" for = "go"></label>
+        </div>
+
+        <span> Checked: {{ optionOne }}, {{ optionTwo }} </span>
+
+      </div>
+
+      <button class = "btn-primary" type = "submit" value = "FIND CAFE">
+        <p> FIND CAFE </p>
+      </button>
+    </form>
+
+    <div id="instafeed">
+    </div>
+
+
+
+  </div>
+</template>
+
+<script>
+import Instafeed from 'instafeed.js'
+
+let query = "coffee nice atmosphere";
+
+export default {
+  name: 'hello',
+  data () {
+    return {
+      optionOne: "Chill",
+      optionTwo: "Stay"
+    }
+  },
+  methods: {
+    submit: function(){
+      if (this.$data.optionOne == "Chill") {
+
+      } else {
+        query = "coffee wifi outlets";
+
+      }
+
+      if (this.$data.optionTwo == "Stay") {
+
+      } else {
+
+      }
+    }
+  }
+}
+ var feed = new Instafeed({
+       get: 'tagged',
+       tagName: 'awesome',
+       clientId: '53726708.1677ed0.de68285baef341d580f7b9ea226cf534'
+   });
+   feed.run();
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang = "scss" scoped>
+@keyframes pulse {
+  from {
+    transform: scale3d(1, 1, 1);
+  }
+
+  50% {
+    transform: scale3d(1.05, 1.05, 1.05);
+  }
+
+  to {
+    transform: scale3d(1, 1, 1);
+  }
+}
+
+.body {
+  align-self: center;
+
+  max-width: 1024px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+}
+
+#selection {
+  width: 800px;
+
+  input {
+    -webkit-appearance:none;
+     -moz-appearance:none;
+          appearance:none;
+          display: none;
+  }
+
+  input:checked + .btn-image {
+    -webkit-filter: none;
+       -moz-filter: none;
+            filter: none;
+  }
+
+}
+
+.button-set {
+  display: flex;
+  justify-content: space-between;
+}
+
+.btn-image {
+  cursor:pointer;
+  background-size:contain;
+  background-repeat:no-repeat;
+  display:inline-block;
+  width:108px;
+  height: 108px;
+  -webkit-transition: all 300ms ease-in;
+        -moz-transition: all 300ms ease-in;
+             transition: all 300ms ease-in;
+     -webkit-filter: brightness(1.5) grayscale(1) opacity(.5);
+        -moz-filter: brightness(1.5) grayscale(1) opacity(.5);
+             filter: brightness(1.5) grayscale(1) opacity(.5);
+
+  &:hover, &:active {
+    -webkit-filter: brightness(1) grayscale(0) opacity(.5);
+       -moz-filter: brightness(1) grayscale(0) opacity(.5);
+            filter: brightness(1) grayscale(0) opacity(.5);
+  }
+  &:not(:active){
+    animation: pulse 1s;
+  }
+}
+
+.chill {
+  background-image: url("../assets/Button/Icon/Chill@2x.png");
+}
+.productive {
+  background-image: url("../assets/Button/Icon/Productive@2x.png");
+}
+.stay {
+  background-image: url("../assets/Button/Icon/Stay@2x.png");
+}
+.go {
+  background-image: url("../assets/Button/Icon/Go@2x.png");
+}
+
+</style>

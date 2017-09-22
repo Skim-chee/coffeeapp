e07fdd15b3c68fd5263032b2ee351ad1ce06952f
skim-chee <kim.seean@gmail.com>
2017-09-22 02:04:43 -0400

    ADDED: set up basic express server TODO: finish up yelp api calls, have it communicate with Vue to send&recieve info, work on fb/ig

--- server.js
+++ server.js
@@ -6,13 +6,49 @@ var express = require('express'),
       path = require('path'),
       bodyParser = require('body-parser'),
       cors = require('cors'),
-      mongoose = require('mongoose');
+      mongoose = require('mongoose'),
+      axios = require('axios'),
+      yelp = require('yelp-fusion');
 
       const app = express();
       app.use(express.static('public'));
       app.use(bodyParser.json());
       app.use(cors());
       var port = process.env.PORT || 4000;
+
+      var client;
+
+      const token = yelp.accessToken(process.env.YELP_KEY, process.env.YELP_SECRET).then(response =>{
+          console.log(response.jsonBody.access_token);
+          client = yelp.client(response.jsonBody.access_token);
+        }).catch(e => {
+          console.log(e);
+        });
+
+      app.get('/yelpSearch', (req, res) => {
+        client.search({
+          term:'Coffee',
+          location: 'manhattan, ny'
+        }).then(response => {
+          const resJson = response.jsonBody
+          let count = 0;
+          let resVal;
+
+          while (resVal === undefined) {
+            if (resJson.businesses[count].rating >= 4) {
+              console.log(resJson.businesses[0].name);
+              res.send(resJson.businesses[0].name);
+              resVal = resJson.businesses[0].name;
+              console.log(resJson.businesses[count].rating);
+            } else {
+              count++;
+            }
+          }
+        }).catch(e => {
+          console.log(e);
+        });
+      })
+
 
       var server = app.listen(port, () => {
         console.log('Listening on port ' + port);

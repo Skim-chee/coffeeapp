97ebee2de59c3c43ee2d07c57b61a01bc3997a4d
skim-chee <kim.seean@gmail.com>
2017-09-25 04:38:00 -0400

    Test NGINX to proxy to https

--- server.js
+++ server.js
@@ -2,9 +2,10 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').load();
 }
-var express = require('express')
+var express = require('express'),
       http = require('http'),
       https = require('https'),
+      forceSsl = require('express-force-ssl'),
       fs = require('fs'),
       path = require('path'),
       bodyParser = require('body-parser'),
@@ -13,9 +14,8 @@ var express = require('express')
       yelpSearch = require('./src/routes/yelpSearch'),
       fbSearch = require('./src/routes/fbSearch');
 
-
-
       const app = express();
+      app.use(forceSsl);
       app.use(express.static('public'));
       app.use(bodyParser.json());
       app.use(bodyParser.urlencoded({ extended: true }))
@@ -25,17 +25,17 @@ var express = require('express')
       var port = process.env.PORT || 4000;
 
       // If production, sets certificates for https
-      var cert = fs.readFileSync('/etc/ssl/certs/nginx-selfsigned.crt');
-      var key = fs.readFileSync('/etc/ssl/private/nginx-selfsigned.key');
-      var options = {
-        key: key,
-        cert: cert
-      };
-      https.createServer(options, app).listen(4330);
       // if (process.env.NODE_ENV === 'production') {
-      //
-      // } else {
-      //   var server = app.listen(port, () => {
-      //     console.log('Listening on port ' + port);
-      //   });
+      //   var key = fs.readFileSync('encryption/private.key');
+      //   var cert = fs.readFileSync( 'encryption/primary.crt' );
+      //   var ca = fs.readFileSync( 'encryption/intermediate.crt' );
+      //   var options = {
+      //     key: key,
+      //     cert: cert,
+      //     ca: ca
+      //   };
+      //   https.createServer(options, app).listen(4330);
       // }
+      http.createServer(app).listen(port, () => {
+        console.log('Listening on port ' + port);
+      });

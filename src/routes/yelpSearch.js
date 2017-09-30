332a3df24e66bc7fee91f5cc0256feff045e431e
skim-chee <kim.seean@gmail.com>
2017-09-29 21:37:59 -0400

    Reverting state to 3adfae7e5b3b70310dae64de0ae8a1b8b5471f5f

--- src/routes/yelpSearch.js
+++ src/routes/yelpSearch.js
@@ -16,10 +16,6 @@ function randomGenerator (numBus) {
   return Math.floor(Math.random() * numBus);
 }
 
-function yelpSearch(query, lat, lon, rad) {
-
-}
-
 router.post('/yelpSearch', (req, res) => {
   let query = req.body.data.query;
   let coords = req.body.data.coords.split(',');
@@ -29,14 +25,10 @@ router.post('/yelpSearch', (req, res) => {
   let rad = 9000;
   // If stay, shows radius of 1 mile, otherwise 6 miles
   if (req.body.data.stay) {
-    rad = 500;
+    rad = 1500;
   } else {
     rad = 9000;
   }
-  console.log("HI");
-  //TODO: Increase search radius if nothing found
-  //TODO: Probably put search in own function, put into a while loop
-  //TODO: Return 404 on errors, alert on page of error
   // Check to make sure query and coords got passed through
   if (query || coords === undefined) {
     client.search({
@@ -60,7 +52,7 @@ router.post('/yelpSearch', (req, res) => {
         // Only marks cafes with a 4 or higher rating
         if (randBusiness.rating >= 4) {
           // Returns the selected business name
-          return res.status(200).json(resJson.businesses[randBus]);
+          res.status(200).json(resJson.businesses[randBus]);
           resVal = resJson.businesses[randBus].name;
         } else {
           // Loops with another randomly generated business number
@@ -68,7 +60,6 @@ router.post('/yelpSearch', (req, res) => {
         }
       }
     }).catch(e => {
-      return res.status(404).json({data: "Failed to find :("});
       console.log(e);
     });
   }

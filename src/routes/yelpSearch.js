c512b445611c232eab3488da543d8a156010e686
skim-chee <kim.seean@gmail.com>
2017-09-30 03:18:03 -0400

    changed logic from while to for loop for yelp search

--- src/routes/yelpSearch.js
+++ src/routes/yelpSearch.js
@@ -28,13 +28,12 @@ function yelpS (query, lat, lon, rad, res, times) {
     console.log("Yelp search #:" + times);
     const resJson = response.jsonBody
     // Variable to check when response value found
-    let resVal;
 
     // Generates a random business number upto 50
     let count = resJson.businesses.length;
     let randBus = randomGenerator(count);
-
-    while (resVal === undefined) {
+    for (var c = 0; c < count; c++) {
+      console.log("looping: " + c);
       let randBusiness = resJson.businesses[randBus]
       // Only marks cafes with a 4 or higher rating
       if (randBusiness.rating >= 4) {
@@ -46,6 +45,7 @@ function yelpS (query, lat, lon, rad, res, times) {
         randBus = randomGenerator(count);
       }
     }
+    return res.status(200).json({name: "Could not find :("});
   }).catch(e => {
     // console.log(e);
     if (times < 6) {
